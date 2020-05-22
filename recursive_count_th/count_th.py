c4693efdfaa750b6e6e8af050630f38ec061ebab
Tyler Christian <tylerc71197@gmail.com>
2020-05-22 12:50:50 -0400

    test for the count_th.py working and passing

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,13 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
-    
-    pass
+    result = 0
+    if len(word) < 2:
+        return 0
+    # if the first and second index of a word are t or h
+    # increment the result
+    if word[0] == 't' and word[1] == 'h':
+        result += 1
+    # add both the result and count of the word togethere in order to get final outcome
+    return result + count_th(word[1:])
+
